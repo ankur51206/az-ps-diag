95a7e75f88ff87566ee3162279f8bca31213b63a
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2022-10-07 22:13:27 +0530

    Update post.ps1

--- post.ps1
+++ post.ps1
@@ -3,7 +3,7 @@ param(
     # Add Resource ID below if you dont want to add multiple database at a time
     # $resourceID = "", 
     $diagName = "evhnsdiag1",
-    # Add your log analytics workspace ID here at below
+    # Add your log analytics workspace name here at below
     $laws = "samplelaws",
     # add your eventhub rule ID here
     $eventRule = "/subscriptions/f3d20c9f-3cb5-45df-b6a8-32f7f4e3d1b6/resourceGroups/sample-1/providers/Microsoft.EventHub/namespaces/eventnamespaceankur/authorizationRules/RootManageSharedAccessKey",
