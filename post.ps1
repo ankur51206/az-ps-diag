2a271f0d85584cef51d57e452cb0d66beed5d839
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2022-10-11 00:09:28 +0530

    Update post.ps1

--- post.ps1
+++ post.ps1
@@ -2,8 +2,8 @@
 param(
     # Add Resource ID below if you dont want to add multiple database at a time
     # $resourceID = "", 
-    $diagName = "evhnsdiag1",
-    # Add your log analytics workspace name here at below
+    $diagName = "ankurdiag",
+    # Add your log analytics workspace ID here at below
     $laws = "samplelaws",
     # add your eventhub rule ID here
     $eventRule = "/subscriptions/f3d20c9f-3cb5-45df-b6a8-32f7f4e3d1b6/resourceGroups/sample-1/providers/Microsoft.EventHub/namespaces/eventnamespaceankur/authorizationRules/RootManageSharedAccessKey",
@@ -11,7 +11,10 @@ param(
     $eventHubName = "myeventhubankur",
     [switch]$set
 )
-
+              
+$metrics = '[{\"category\":\"AllMetrics\",\"retentionPolicy\":{\"days\":0,\"enabled\":false},\"enabled\":true}]'
+$logs = '[{\"category\":\"PostgreSQLLogs\",\"retentionPolicy\":{\"days\":0,\"enabled\":false},\"enabled\":true}]'
+            
 foreach ($resourceID in Get-Content .\file.txt) {
-    az monitor diagnostic-settings create --resource $resourceID -n $diagName --workspace $laws --event-hub-rule $eventRule --event-hub $eventHubName --logs '[{""category"": ""PostgreSQLLogs"",""enabled"": true}]'    
+    az monitor diagnostic-settings create --resource $resourceID -n $diagName --workspace $laws --event-hub-rule $eventRule --event-hub $eventHubName --logs $logs --metrics $metrics   
 }
